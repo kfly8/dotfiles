ac787f8f02f5dde6ec20cbacffdeebc18b420b66
Kenta, Kobayashi <kentafly88@gmail.com>
2012-09-17 18:53:26 +0900

    不要なシンボリックリンクができてしまうのを修正

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 cd $(dirname $0)
 
-dotfiles=(vim vimrc zshrc zsh gitconfig gitmessage gitignore screenrc)
+dotfiles=(vimrc vim zshrc zsh gitconfig gitmessage gitignore screenrc)
 for (( i = 0; i < ${#dotfiles[*]}; i++ ))
 {
   dotfile=${dotfiles[i]}
-  ln -fs "$PWD/$dotfile" $HOME/.$dotfile
+  path="$PWD/$dotfile"
+  target_path=$HOME/.$dotfile
+  if [ -L $target_path ]; then
+      rm $target_path
+  fi
+  ln -s $path $target_path
 }
