aba536e6f803c149af2c922fba50228bead763e4
Kenta, Kobayashi <kentafly88@gmail.com>
2017-05-05 22:35:25 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 cd $(dirname $0)
 
-dotfiles=(vimrc vim zshrc zsh gitconfig gitmessage gitignore screenrc tmux.conf sheets sheet_snippets peco editorconfig)
-for (( i = 0; i < ${#dotfiles[*]}; i++ ))
-{
-  dotfile=${dotfiles[i]}
-  path="$PWD/$dotfile"
-  target_path=$HOME/.$dotfile
-  ln -s $path $target_path
-}
+ln -s $PWD/config       $HOME/.config
+ln -s $PWD/vimrc        $HOME/.vimrc
+ln -s $PWD/vim          $HOME/.vim
+ln -s $PWD/gitconfig    $HOME/.gitconfig
+ln -s $PWD/gitmessage   $HOME/.gitmessage
+ln -s $PWD/gitignore    $HOME/.gitignore
+ln -s $PWD/tmux.conf    $HOME/.tmux.conf
+ln -s $PWD/peco         $HOME/.peco
+ln -s $PWD/editorconfig $HOME/.editorconfig
+ln -s $PWD/proverc      $HOME/.proverc
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle install
